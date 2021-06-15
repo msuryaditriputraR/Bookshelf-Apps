04d957b1809d12cac5f15d6a679f4fe8667b767b
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-06-15 21:18:17 +0800

    create card book

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,8 +1,50 @@
+// NOTE: Membuat Card
+const makeCardBook = (title, author, year, isCompleted) => {
+    const textTitle = document.createElement('h2').classList.add('text-title');
+    textTitle.innerText = title;
+
+    const textSubtitle = document
+        .createElement('p')
+        .classList.add('text-subtitle');
+    textSubtitle.innerText = `${author} | ${year}`;
+
+    const textHeader = document
+        .createElement('div')
+        .classList.add('text-header');
+    textHeader.append(textTitle, textSubtitle);
+
+    const textIsCompleted = document
+        .createElement('p')
+        .classList.add('text-isCompleted');
+
+    if (isCompleted) {
+        textIsCompleted.classList.add('completed');
+        textIsCompleted.innerText = 'Completed';
+    } else {
+        textIsCompleted.classList.add('uncompleted');
+        textIsCompleted.innerText = 'Uncompleted';
+    }
+
+    const cardHeader = document
+        .createElement('div')
+        .classList.add('card-header');
+
+    cardHeader.append(textHeader, textIsCompleted);
+
+    const cardFooter = document
+        .createElement('div')
+        .classList.add('card-footer');
+
+    if (isCompleted) cardFooter.append(unFinishButton(), deleteButton());
+    else cardFooter.append(finishButton(), deleteButton());
+
+    const card = document.createElement('div').classList.add('card');
+    card.append(cardHeader, cardFooter);
+
+    return card;
+};
+
 // NOTE: Membuat abstrak Button
-{
-    /* <button class="btn btn-green">Finish</button> */
-}
-//         <button class="btn btn-red">Delete</button>
 const createButton = (buttonColorClass, eventListener, textBtn) => {
     const button = document
         .createElement('button')
