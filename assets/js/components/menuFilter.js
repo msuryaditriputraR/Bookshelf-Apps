cef0d4ea9547e0e9e18ed0b65ceedb3d317dc3f5
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-06-20 00:01:05 +0800

    move the menu Filter component to a separate file"

---
+++ assets/js/components/menuFilter.js
@@ -0,0 +1,19 @@
+import { showCardFilter } from './../dom.js';
+
+// NOTE: Filter
+const btnFilter = document.querySelector('.dropdown-filter button');
+const filterMenuContainer = document.querySelector('.dropdown-filter-menu');
+const filterMenu = filterMenuContainer.querySelectorAll('input');
+
+btnFilter.addEventListener('click', () => {
+    filterMenuContainer.classList.toggle('show');
+});
+
+filterMenuContainer.addEventListener('click', event => {
+    filterMenu.forEach(menu => {
+        if (event.target == menu) {
+            const valueFilter = event.target.value;
+            showCardFilter(valueFilter);
+        }
+    });
+});
