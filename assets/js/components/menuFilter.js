5c91a71502ea0e79a2d509023b9d0d5c1d0aa7b9
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-06-20 15:11:04 +0800

    create eventlistener when if click anything (except btnfilter, menuFilter, and FilterMenuContainer)  close the menu filter

--- assets/js/components/menuFilter.js
+++ assets/js/components/menuFilter.js
@@ -3,17 +3,32 @@ import { showCardFilter } from './card.js';
 // NOTE: Filter
 const btnFilter = document.querySelector('.dropdown-filter button');
 const filterMenuContainer = document.querySelector('.dropdown-filter-menu');
-const filterMenu = filterMenuContainer.querySelectorAll('input');
+const filterMenu = filterMenuContainer.querySelectorAll('.menu');
+// const filterMenuInput = filterMenu.querySelectorAll('input');
 
 btnFilter.addEventListener('click', () => {
     filterMenuContainer.classList.toggle('show');
 });
 
-filterMenuContainer.addEventListener('click', event => {
-    filterMenu.forEach(menu => {
-        if (event.target == menu) {
+filterMenu.forEach(menu => {
+    menu.addEventListener('click', event => {
+        const filterMenuInput = menu.querySelector('input');
+        if (event.target == filterMenuInput) {
             const valueFilter = event.target.value;
             showCardFilter(valueFilter);
+            filterMenuContainer.classList.remove('show');
         }
     });
+});
+
+// NOTE: click anything except btnFilter & filterMenuContainer to close menu filter
+document.addEventListener('click', event => {
+    if (event.target != btnFilter) {
+        let isMenu = false;
+        if (event.target == filterMenuContainer) return;
+        filterMenu.forEach(menu => {
+            if (event.target == menu) isMenu = true;
+        });
+        if (!isMenu) filterMenuContainer.classList.remove('show');
+    }
 });
