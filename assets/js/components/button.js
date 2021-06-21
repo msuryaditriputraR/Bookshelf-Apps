b645615ec82f09236609b5aa79947c9e2ba77685
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-06-22 02:43:29 +0800

    refactoring component button to get optional parameters

--- assets/js/components/button.js
+++ assets/js/components/button.js
@@ -2,12 +2,12 @@ import { changeBookToCompleted, changeBookToUnCompleted } from './../books.js';
 import { deleteCardBook } from './card.js';
 
 // NOTE: Membuat abstrak Button
-const createButton = (
+const createButton = ({
     buttonColorClass,
     marginClass,
     eventListener,
     textBtn
-) => {
+}) => {
     const button = document.createElement('button');
     button.classList.add('btn', buttonColorClass, marginClass);
 
@@ -21,35 +21,36 @@ const createButton = (
 
 // NOTE: Membuat button Finish
 const finishButton = () =>
-    createButton(
-        'btn-green',
-        'mr-3',
-        event => {
+    createButton({
+        buttonColorClass: 'btn-green',
+        marginClass: 'mr-3',
+        eventListener: event => {
             changeBookToCompleted(event.target.parentElement.parentElement);
         },
-        'Finish'
-    );
+        textBtn: 'Finish'
+    });
 
 // NOTE: Membuat button Delete
 const deleteButton = () =>
-    createButton(
-        'btn-red',
-        'ml-3',
-        event => {
+    createButton({
+        buttonColorClass: 'btn-red',
+        marginClass: 'ml-3',
+        eventListener: event => {
             const isDelete = confirm('Are you Sure delete this book?');
             if (isDelete)
                 deleteCardBook(event.target.parentElement.parentElement);
         },
-        'Delete'
-    );
+        textBtn: 'Delete'
+    });
 
 // NOTE: Membuat button unfinish
 const unFinishButton = () =>
-    createButton(
-        'btn-grey',
-        'mr-3',
-        event => changeBookToUnCompleted(event.target.parentNode.parentNode),
-        'Unfinish'
-    );
+    createButton({
+        buttonColorClass: 'btn-grey',
+        marginClass: 'mr-3',
+        eventListener: event =>
+            changeBookToUnCompleted(event.target.parentNode.parentNode),
+        textBtn: 'Unfinish'
+    });
 
 export { finishButton, deleteButton, unFinishButton };
